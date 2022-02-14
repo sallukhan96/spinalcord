c168d12bcc2ee989f87e4d087afdd747eb147863
sallukhan96 <salmanicp.edu@gmail.com>
2022-02-14 11:13:33 +0500

    ''

--- lib/Description.dart
+++ lib/Description.dart
@@ -9,6 +9,7 @@ class Description extends StatefulWidget {
       : super(key: key);
 
   @override
+
   _DescriptionState createState() => _DescriptionState();
 }
 
@@ -20,30 +21,32 @@ class _DescriptionState extends State<Description> {
         title: Text("تفصیل"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              widget.Stockfield,
-              textAlign: TextAlign.right,
-              style: TextStyle(fontSize: 30),
-            ),
-            const Text(
-              "تفصیل",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              widget.description,
-              textAlign: TextAlign.right,
-              style: const TextStyle(
-                fontSize: 18,
-                wordSpacing: 4,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.Stockfield,
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 30),
               ),
-            )
-          ],
+              const Text(
+                "تفصیل",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                widget.description,
+                textAlign: TextAlign.right,
+                style: const TextStyle(
+                  fontSize: 18,
+                  wordSpacing: 4,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
