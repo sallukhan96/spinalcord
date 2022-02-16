28e8f6d3cc73e8881d23cb6d8d25f1b0b38dd9fb
saeedanwar <saeedanwar@saeedanwars-MacBook-Pro.local>
2022-02-16 12:23:02 +0500

    jsdcjbd

--- lib/Description.dart
+++ lib/Description.dart
@@ -22,35 +22,39 @@ class _DescriptionState extends State<Description> {
         title: const Text("تفصیل"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                widget.Stockfield ?? ' ',
-                textAlign: TextAlign.right,
-                style: const TextStyle(fontSize: 30),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              for (var i = 0; i < widget.data!.length; i++)
-                _paragraph(
-                  heading: widget.data?[i].heading,
-                  paragraph: widget.data?[i].paragraph,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  widget.Stockfield ?? ' ',
+                  textAlign: TextAlign.right,
+                  style: const TextStyle(fontSize: 30),
                 ),
-              widget.Imgsource == 'null'
-                  ? const SizedBox(
-                      height: 100,
-                      width: 1,
-                    )
-                  : Image(
-                      image: AssetImage(widget.Imgsource!),
-                    )
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                for (var i = 0; i < widget.data!.length; i++)
+                  _paragraph(
+                    heading: widget.data?[i].heading,
+                    paragraph: widget.data?[i].paragraph,
+                  ),
+                widget.Imgsource == 'null'
+                    ? const SizedBox(
+                        height: 100,
+                        width: 1,
+                      )
+                    : Image(
+                        image: AssetImage(widget.Imgsource!),
+                      )
+              ],
+            ),
           ),
         ),
       ),
@@ -58,24 +62,25 @@ class _DescriptionState extends State<Description> {
   }
 
   _paragraph({String? heading, String? paragraph}) {
-    return ListView(shrinkWrap: true,
-        children: [
-      const SizedBox(
-        height: 30,
-      ),
-      Text(
-        heading ?? ' ',
-        textAlign: TextAlign.right,
-        style: const TextStyle(fontSize: 26),
-      ),
-      Text(
-        paragraph ?? ' ',
-        textAlign: TextAlign.right,
-        style: const TextStyle(
-          fontSize: 18,
-          wordSpacing: 4,
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
         ),
-      ),
-    ]);
+        Text(
+          heading ?? ' ',
+          textAlign: TextAlign.right,
+          style: const TextStyle(fontSize: 26),
+        ),
+        Text(
+          paragraph ?? ' ',
+          textAlign: TextAlign.right,
+          style: const TextStyle(
+            fontSize: 18,
+            wordSpacing: 4,
+          ),
+        ),
+      ],
+    );
   }
 }
