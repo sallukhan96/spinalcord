0f7b18c8995414bc61ad7211b5f894893a15f2dc
sallukhan96 <salmanicp.edu@gmail.com>
2022-03-02 11:46:45 +0500

    changes to login page

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -10,77 +10,125 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Center(
-              child: Form(
-                  key: formkey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 150,
-                        width: 200,
-                        child: Image.asset(
-                          'assets/images/urdu-version-150x100.png',
-                          fit: BoxFit.fitWidth,
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Center(
+                child: Form(
+                    key: formkey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
                           height: 150,
-                          width: 100,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty)
-                              return "required";
-                            if (!RegExp(
-                                    r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
-                                .hasMatch(value)) return 'INVALID EMAIL';
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "email",
+                          width: 200,
+                          child: Image.asset(
+                            'assets/images/urdu-version-150x100.png',
+                            fit: BoxFit.fitWidth,
+                            height: 150,
+                            width: 100,
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty && value.length <= 6)
-                              return "password must not null";
-                          },
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "password"),
+                        const SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      SizedBox(height: 25),
-                      Container(
-                        width: 270.0,
-                        height: 45.0,
-                        // ignore: deprecated_member_use
-                        child: RaisedButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          color: Colors.blue,
-                          textColor: Colors.white,
-                          child: Text("Login"),
-                          onPressed: () {
-                            if (formkey.currentState!.validate()) {
-                              formkey.currentState!.save();
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty)
+                                return "required";
+                              if (!RegExp(
+                                      r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+                                  .hasMatch(value)) return 'INVALID EMAIL';
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "email",
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty && value.length <= 6)
+                                return "password must not null";
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "password"),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Container(
+                          width: 270.0,
+                          height: 45.0,
+                          // ignore: deprecated_member_use
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Text("Login"),
+                            onPressed: () {
+                              if (formkey.currentState!.validate()) {
+                                formkey.currentState!.save();
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Homepage()),
+                                );
+
+                                return null;
+                              }
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 270.0,
+                          height: 45.0,
+                          // ignore: deprecated_member_use
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Text("Create Account"),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignUp()),
+                              );
+
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 270.0,
+                          height: 45.0,
+                          // ignore: deprecated_member_use
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: Text("Login As A Guest"),
+                            onPressed: () {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
@@ -88,62 +136,12 @@ class LoginScreen extends StatelessWidget {
                               );
 
                               return null;
-                            }
-                          },
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            width: 270.0,
-                            height: 45.0,
-                            child: RaisedButton(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30)),
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                              color: Colors.blue,
-                              textColor: Colors.white,
-                              child: Text("Create Acoount"),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SignUp()),
-                                );
-
-                                return null;
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: InkWell(
-                            child: const Text(
-                              "LogIn As A Guest!",
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            onTap: () {
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //       builder: (context) => SignUp()),
-                              // );
                             },
                           ),
                         ),
-                      ),
-                    ],
-                  ))),
+                      ],
+                    ))),
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         //fontFamily: 'NotoNastaliqUrdu',
       ),
-      home: Homepage(),
+      home: SplashScreen(),
     );
   }
 }
